3ebf34809696c88133dcdde53f8c919358332b7f
Kellen Proctor <kellenproctor@gmail.com>
2023-04-07 15:48:37 -0700

    Add some quick style

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
-  printWidth: 100,
+  printWidth: 120,
 };
 
 module.exports = config;
