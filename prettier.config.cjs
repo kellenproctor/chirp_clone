6b5fd3d6ca0bca483c6a332a673e0730b3b0c2f4
Kellen Proctor <kellenproctor@gmail.com>
2023-04-08 11:27:29 -0700

    get author data from clerk for each post

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
-  printWidth: 120,
+  printWidth: 100,
 };
 
 module.exports = config;
